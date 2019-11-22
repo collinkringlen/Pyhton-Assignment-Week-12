a20cfebe0c583991fc23bcb315c0133ef7aeb1b0
Collin Kringlen <collin@csm-wl-dhcp-197-227.mines.edu>
2019-11-21 18:06:27 -0700

    second function completed

--- week12.py
+++ week12.py
@@ -4,5 +4,13 @@
 #Week 12-Part A
 
 
+
 def PrintOutput(hello):
     print("OUTPUT", hello)
+
+def LoadFile(filestring):
+    file = open(filestring, 'r')
+    readfile = file.read().splitlines()
+    printlist = list(readfile)
+    return printlist
+    file.close()
