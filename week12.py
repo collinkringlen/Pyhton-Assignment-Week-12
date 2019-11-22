b1d669923b32b975b67bbae9c6b25420ae4c88ff
Collin Kringlen <collin@csm-wl-dhcp-197-227.mines.edu>
2019-11-21 18:10:37 -0700

    thrid funtion completed

--- week12.py
+++ week12.py
@@ -14,3 +14,9 @@ def LoadFile(filestring):
     printlist = list(readfile)
     return printlist
     file.close()
+def UpdateString(str1, str2, a):
+    str1 = list(str1)
+    str1[a]=str2
+    str1 = ''.join(str1)
+    finalstr = str(str1)
+    print("OUTPUT", finalstr)
